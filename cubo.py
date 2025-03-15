a9ce750f25d264085dc5889db5a1323805edf648
Sebastian Ospino <emilio.sebastian.ospino@gmail.com>
2025-03-15 15:39:04 -0600

    antirhorario

--- cubo.py
+++ cubo.py
@@ -92,8 +92,58 @@ class Cube:
             self.face["Bottom"][2][2 - i] = self.face["Left"][i][0]
             self.face["Left"][i][0] = temp[i]
     
-    def rotate_face_anticlockwise(self, face_name):
-        self.rotate_face_counterclockwise(face_name)
+    def rotate_top_counterclockwise(self):
+        self.rotate_face_counterclockwise("Top")
+        temp = self.face["Front"][0]
+        self.face["Front"][0] = self.face["Left"][0]
+        self.face["Left"][0] = self.face["Back"][0]
+        self.face["Back"][0] = self.face["Right"][0]
+        self.face["Right"][0] = temp
+        
+    def rotate_bottom_counterclockwise(self):
+        self.rotate_face_counterclockwise("Bottom")
+        temp = self.face["Front"][2]
+        self.face["Front"][2] = self.face["Right"][2]
+        self.face["Right"][2] = self.face["Back"][2]
+        self.face["Back"][2] = self.face["Left"][2]
+        self.face["Left"][2] = temp
+
+    def rotate_left_counterclockwise(self):
+        self.rotate_face_counterclockwise("Left")
+        temp_col = [self.face["Top"][i][0] for i in range(3)]
+        for i in range(3):
+            self.face["Top"][i][0] = self.face["Front"][i][0]
+            self.face["Front"][i][0] = self.face["Bottom"][i][0]
+            self.face["Bottom"][i][0] = self.face["Back"][2 - i][2]
+            self.face["Back"][2 - i][2] = temp_col[i]
+
+    def rotate_right_counterclockwise(self):
+        self.rotate_face_counterclockwise("Right")
+        temp_col = [self.face["Top"][i][2] for i in range(3)]
+        for i in range(3):
+            self.face["Top"][i][2] = self.face["Back"][2 - i][0]
+            self.face["Back"][2 - i][0] = self.face["Bottom"][i][2]
+            self.face["Bottom"][i][2] = self.face["Front"][i][2]
+            self.face["Front"][i][2] = temp_col[i]
+
+    def rotate_front_counterclockwise(self):
+        self.rotate_face_counterclockwise("Front")
+        temp = [self.face["Top"][2][i] for i in range(3)]
+        for i in range(3):
+            self.face["Top"][2][i] = self.face["Right"][i][0]
+            self.face["Right"][i][0] = self.face["Bottom"][0][i]
+            self.face["Bottom"][0][i] = self.face["Left"][2 - i][2]
+            self.face["Left"][2 - i][2] = temp[i]
+
+    def rotate_back_counterclockwise(self):
+        self.rotate_face_counterclockwise("Back")
+        temp = [self.face["Top"][0][i] for i in range(3)]
+        for i in range(3):
+            self.face["Top"][0][i] = self.face["Left"][i][0]
+            self.face["Left"][i][0] = self.face["Bottom"][2][2 - i]
+            self.face["Bottom"][2][2 - i] = self.face["Right"][i][2]
+            self.face["Right"][i][2] = temp[i]
+        
     
     def print_cube(self):
         for face, grid in self.face.items():
@@ -106,7 +156,7 @@ cubo = Cube()
 print("Initial state")
 cubo.print_cube()
 print("\n")
-cubo.rotate_top_clockwise()
+cubo.rotate_top_counterclockwise()
 print("After rotating the top face")
 cubo.print_cube()
 print("\n")
